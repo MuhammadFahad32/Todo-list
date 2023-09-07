aadb06c1af73603f7d4282441ddd5a6d827c1d64
unknown <mfahad8680292@gmail.com>
2023-09-07 17:08:22 +0500

    todo

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+// import Header from "@/components/Header";
+
+import Header from "@/componenets/Header"
+import TodoList from "@/componenets/TodoList"
+// import TodoList from "@/components/TodoList";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <TodoList />
+    </main>
+  )
+}
